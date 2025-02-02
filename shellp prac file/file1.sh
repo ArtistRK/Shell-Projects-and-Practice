0734fecefdc7f280a1c121edb7117eb2df08c2f4
Rishab Kanoujia <108139937+ArtistRK@users.noreply.github.com>
2025-02-02 18:54:49 -0500

    Update file1.sh

--- shellp prac file/file1.sh
+++ shellp prac file/file1.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+>>comment
+billo bage biliya da ki krengi
+kii krengi dsda 
+this is multi line comment
+comment
+
+
+echo"hello billu"
+
+a=10
+name1="goat"
+
+echo("My name is $name1 and mai $a da aa")
